80becab638491dd1fb5013427cd0d0fb400086cf
S.O <shakirat.odunsi@gmail.com>
2018-11-01 15:20:57 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,7 @@
 def using_until
   levitation_force = 6
+  levitation_force += 1
   until levitation_force = 10
-    print "Wingardium Leviosa"
-      levitation_force += 1
+    puts "Wingardium Leviosa"
 end
 end
-using_until
